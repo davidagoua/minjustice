1779de7f9976c09bfe0f43b134414ed05887359a
root <root@email>
2022-07-22 09:59:56 +0000

    update doc verify

--- resources/views/form/recap_casier.blade.php
+++ resources/views/form/recap_casier.blade.php
@@ -1,8 +1,37 @@
 <div>
-    <div class="text-xl">Resumé de la demande de casier judiciaire</div>
-    <table class="table w-full">
-        <tr>
 
-        </tr>
-    </table>
+    <div class="row ">
+
+        <div class="w-8/12 p-4 border-2 border-blue-200">
+            <table class="table w-full table-borderless">
+                <tr>
+                    <th style="border-width: 0px">Nom & présnoms</th>
+                    <td style="border-width: 0px">
+                        {{ $user->fullName }}
+                    </td>
+                </tr>
+                <tr>
+                    <th style="border-width: 0px">Date & lieu de naissance</th>
+                    <td style="border-width: 0px">
+                        {{ $user->date_naissance }} à {{ $user->lieu_naissance }}
+                    </td>
+                </tr>
+
+                <tr>
+                    <th style="border-width: 0px">Date de la demande</th>
+                    <td style="border-width: 0px">{{ now() }}</td>
+                </tr>
+
+                <tr>
+                    <th style="border-width: 0px">Juridiction</th>
+                    <td style="border-width: 0px">{{ $user->ville }}</td>
+                </tr>
+                <tr >
+                    <td columnspan="2" class="text-xl text-green-800">
+                        Montant: {{ $document->montant }} FCFA
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </div>
